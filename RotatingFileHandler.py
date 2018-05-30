ff793fc2da6be14b3b3ce63cbc82dd781cee66c0
tetsuya.hirata <tetsuya.hirata@CL-180042-MAC.local>
2018-05-30 17:34:31 +0900

    Add autopep8

--- RotatingFileHandler.py
+++ RotatingFileHandler.py
@@ -1,23 +1,27 @@
-import logging                                                                  
-import logging.handlers  
-from flask import Flask                                                      
-                                                                                  
-app = Flask(__name__)     
+import logging
+import logging.handlers
+from flask import Flask
+
+app = Flask(__name__)
 
 # Add RotatingFileHandler to Flask Logger
-handler = logging.handlers.RotatingFileHandler("test.log", "a+", maxBytes=3000, backupCount=5)
-handler.setLevel(logging.INFO) 
-handler.setFormatter(logging.Formatter('[%(asctime)s] %(levelname)s in %(module)s: %(message)s'))
+handler = logging.handlers.RotatingFileHandler(
+    "test.log", "a+", maxBytes=3000, backupCount=5)
+handler.setLevel(logging.INFO)
+handler.setFormatter(logging.Formatter(
+    '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'))
 app.logger.addHandler(handler)
 
-@app.route('/')                                                                 
-def index():                                                                    
-    app.logger.debug('debug')                                                   
-    app.logger.info('info')                                                     
-    app.logger.warn('warn')                                                     
-    app.logger.error('error')                                                   
-    app.logger.critical('critical')                                                                                                                         
-    return "logging"                                                            
-                                                                                                                                                    
-if __name__ == '__main__':                                                      
-    app.run(debug=True)     
+
+@app.route('/')
+def index():
+    app.logger.debug('debug')
+    app.logger.info('info')
+    app.logger.warn('warn')
+    app.logger.error('error')
+    app.logger.critical('critical')
+    return "logging"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- big_small_app.py
+++ big_small_app.py
@@ -1,17 +1,20 @@
 import pytest
 import time
 
+
 @pytest.mark.small
 def test_1():
-	time.sleep(0.1)
-	assert "aaa" == "bbb"
+    time.sleep(0.1)
+    assert "aaa" == "bbb"
+
 
 @pytest.mark.small
 def test_2():
-	time.sleep(0.1)
-	assert "bbb" == "bbb"
+    time.sleep(0.1)
+    assert "bbb" == "bbb"
+
 
 @pytest.mark.large
 def test_3():
-	time.sleep(10)
-	assert "aaa" == "bbb"
+    time.sleep(10)
+    assert "aaa" == "bbb"
